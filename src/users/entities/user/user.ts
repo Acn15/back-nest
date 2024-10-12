764eaa6a015b75ba9b2dba205832d1459bf00d3a
andressouza28 <andre-souza@drytelecom.com.br>
2024-10-12 03:27:14 -0300

    refac: change entity

--- src/users/entities/user/user.ts
+++ src/users/entities/user/user.ts
@@ -1,4 +1,5 @@
 import { Exclude } from 'class-transformer';
+import { IsString, Length, Matches } from 'class-validator';
 import {
   Entity,
   Column,
@@ -13,16 +14,21 @@ export class User {
   id: number;
 
   @Column()
+  @IsString()
   name: string;
 
   @Column({ unique: true })
   email: string;
 
   @Column({ unique: true })
+  @IsString()
+  @Length(11, 11, { message: 'O CPF deve ter 11 dígitos' })
+  @Matches(/^[0-9]+$/, { message: 'O CPF deve conter apenas números' })
   document: string;
 
   @Column()
   @Exclude()
+  @IsString()
   password: string;
 
   @CreateDateColumn({ type: 'timestamp' })
